81f9fbb537218ab9b5c5ebc8c86e813cab0826e4
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-01-12 13:48:39 +0000

    feat: created song model

--- models/song_model.py
+++ models/song_model.py
@@ -1,2 +1,6 @@
-class Music:
-    def __init__(self, ):
+class Song:
+    def __init__(self, song_id, song_name, singer_id, genre_id):
+        self.song_id = song_id
+        self.song_name = song_name
+        self.singer_id = singer_id
+        self.genre_id = genre_id
