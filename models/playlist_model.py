e5b2fc87ad698f4ec58c0b29e4c0377786d1b80d
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-01-12 13:50:05 +0000

    feat: created playlist model

---
+++ models/playlist_model.py
@@ -0,0 +1,6 @@
+class Playlist:
+    def __init__(self, playlist_id, playlist_name, playlist_note, added_songs):
+        self.playlist_id = playlist_id
+        self.playlist_name = playlist_name
+        self.playlist_note = playlist_note
+        self.added_songs = added_songs
